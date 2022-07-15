0697fbd9a089fc425f63ce9e964baf967293f8b6
Laerson <laersonsv@DESKTOP-GHVISB4.localdomain>
2022-07-14 22:11:28 -0300

    Add exercises of chapter 2, of Modern C

---
+++ ComputerScience/ProgrammingLanguages/C/C_Programming_A_Modern_Approach_2nd_Edition/chapter2/exercises/ex4.c
@@ -0,0 +1,14 @@
+/* This is only an example */
+
+#include <stdio.h>
+
+int main(void) 
+{
+    int i, j, k;
+    float a, b, c;
+
+    printf("%d %d %d\n", i, j, k);
+    printf("%f %f %f\n", a, b, c);
+
+    return 0;
+}
