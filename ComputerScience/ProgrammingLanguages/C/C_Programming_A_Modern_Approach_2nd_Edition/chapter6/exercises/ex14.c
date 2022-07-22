ee3517f5086863433e57adcae23168532566e2c3
Laerson <laerson.verissimo@ccc.ufcg.edu.br>
2022-07-21 22:34:03 -0300

    Add exercises from chapter 6

---
+++ ComputerScience/ProgrammingLanguages/C/C_Programming_A_Modern_Approach_2nd_Edition/chapter6/exercises/ex14.c
@@ -0,0 +1,2 @@
+if (n % 2 == 0) // The issue was the ";" at the end
+    printf("n is even\n");
---
+++ ComputerScience/ProgrammingLanguages/C/C_Programming_A_Modern_Approach_2nd_Edition/chapter6/exercises/ex4.c
@@ -0,0 +1,1 @@
+c). Because on the first two, the increment is made at the end of each loop, where in c) the increment is made during the condition check step
